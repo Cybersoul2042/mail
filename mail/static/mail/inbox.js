a1b6ea8e7fe12e901231b7b87d8335bf9ac4f324
Cybersoul2042 <amirsekiroguru@gmail.com>
2023-07-30 15:50:20 +0330

    Add Load Mail Page Function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -18,7 +18,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
-
+  document.querySelector('#email-page').style.display = 'none';
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
@@ -32,6 +32,7 @@ function load_mailbox(mailbox)
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-page').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -41,13 +42,13 @@ function load_mailbox(mailbox)
   .then(emails => {
     emails.forEach(email => {
       let ePlace = document.createElement('div');
-
+      ePlace.className = email['read'] ? "email-list-item-read" : "email-list-item-unread";
       ePlace.innerHTML = `<span class="sender col-3"> <b>${email['sender']}</b> </span>
                           <span class="subject col-6"> ${email['subject']} </span>
                           <span class="timestamp col-3"> ${email['timestamp']} </span>`;
 
       ePlace.addEventListener('click', () => load_mailPage(email['id']));
-      document.querySelector('#emails-view').append(ePlace);
+      document.querySelector('#emails-view').appendChild(ePlace);
     });
   });
 }
@@ -75,13 +76,29 @@ function load_mailPage(id){
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#email-page').style.display = 'block';
 
-  fetch('/emails/' + id)
-  .then(response => response.json)
-  .then(emails => {
-    
+  console.log(id)
+
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+    const emailView = document.querySelector('#email-page');
+
+    console.log(email)
+
+    emailView.innerHTML = `<h3>${email['subject']}</h3>
+                           <h4>From: ${email['sender']}</h4>
+                           <h4>To: ${email['recipients']}</h4>
+                           <p>${email['body']}</p>
+                           <span><b>${email['timestamp']}</b></span>`;
+    readArchive(email['id']);
 
   })
   .catch(error => {
     console.log('Error:', error);
   });
+}
+
+function readArchive(id)
+{
+  
 }
