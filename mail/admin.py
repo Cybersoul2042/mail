a2f93af72e3130cde08782b7bacd06c4c619ebc3
Cybersoul2042 <amirsekiroguru@gmail.com>
2023-07-31 22:07:01 +0330

    Done

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Email, User
+
 # Register your models here.
+class EmailAdmin(admin.ModelAdmin):
+    email_list = ("user","sender","recipients","subject","body","timestamp")
+
+class UserAdmin(admin.ModelAdmin):
+    user_list = ("email", "password")
+
+admin.site.register(Email, EmailAdmin)
+admin.site.register(User, UserAdmin)
